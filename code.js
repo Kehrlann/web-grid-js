547fc3c2b70fa80f6fc29341ec118aa6028cbe86
Daniel Garnier-Moiroux <git@garnier.wf>
2022-02-13 22:04:07 +0100

    move q2 to q5

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Initial clean up. DO NOT REMOVE.
+  initialCleanup();
+
   // Hey! Pssst! In here ...
 });
+
+/**
+ * Cleans up the document so that the exercise is easier.
+ *
+ * There are some text and comment nodes that are in the initial DOM, it's nice
+ * to clean them up beforehand.
+ */
+function initialCleanup() {
+  const nodesToRemove = [];
+  document.getElementById("grid").childNodes.forEach((node, key) => {
+    if (node.nodeType !== Node.ELEMENT_NODE) {
+      nodesToRemove.push(node);
+    }
+  });
+  for (const node of nodesToRemove) {
+    node.remove();
+  }
+}
